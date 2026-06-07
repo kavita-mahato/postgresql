ad618b428939b4eb17006975bfa053befd7469f3
Kavita Mahato <kavitamahato0835@gmail.com>
2026-06-07 17:30:20 +0530

    Running queries on associated data

--- 04photos.sql
+++ 04photos.sql
@@ -9,5 +9,18 @@ INSERT INTO photos (url, user_id)
 VALUES
     ('http://one.jpg', 4);
 
+INSERT INTO photos (url, user_id)
+VALUES
+    ('http://two.jpg', 1),
+    ('http://three.jpg', 4),
+    ('http://four.jpg', 2),
+    ('http://five.jpg', 3),
+    ('http://six.jpg', 3),
+    ('http://seven.jpg', 4);
+
 
 SELECT * FROM photos;
+
+SELECT * FROM photos WHERE user_id = 4;
+
+SELECT url, username FROM photos JOIN users ON users.id = photos.user_id;

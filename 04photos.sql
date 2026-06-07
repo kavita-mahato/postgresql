6c89bd65123353fe7f445885829cc793e5175c48
Kavita Mahato <kavitamahato0835@gmail.com>
2026-06-07 17:21:09 +0530

    Creating foreign key columns

---
+++ 04photos.sql
@@ -0,0 +1,13 @@
+CREATE TABLE photos (
+    id SERIAL PRIMARY KEY,
+    url VARCHAR(200),
+    user_id INTEGER REFERENCES users(id)
+);
+
+
+INSERT INTO photos (url, user_id)
+VALUES
+    ('http://one.jpg', 4);
+
+
+SELECT * FROM photos;

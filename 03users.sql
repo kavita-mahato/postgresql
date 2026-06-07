351ca628bafe3eae68a9d0186e313d805e52df71
Kavita Mahato <kavitamahato0835@gmail.com>
2026-06-07 17:13:00 +0530

    Auto-generated ID's

---
+++ 03users.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50)
+)
+
+INSERT INTO users (username)
+    VALUES
+        ('monahan93'),
+        ('pferrer'),
+        ('chandler'),
+        ('monica');
+
+SELECT * from users;

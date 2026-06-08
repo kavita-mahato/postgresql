4e654c822ab813954c105e888897abf7ba40d991
Kavita Mahato <kavitamahato0835@gmail.com>
2026-06-08 20:54:35 +0530

    JOIN data from different Tables

--- 03users.sql
+++ 03users.sql
@@ -1,14 +1,15 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50)
-)
+CREATE TABLE users(
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(50)
+);
 
-INSERT INTO users (username)
-    VALUES
-        ('monahan93'),
-        ('pferrer'),
-        ('chandler'),
-        ('monica');
 
-SELECT * from users;
+INSERT INTO users (username) 
+VALUES 
+    ('Reyna.Marvin'),
+    ('Micah.Cremin'),
+    ('Alfredo66'),
+    ('Gerard_Mitchell42'),
+    ('Frederique_Donnelly');
 
+SELECT * FROM users;
--- 04photos.sql
+++ 04photos.sql
@@ -1,25 +1,32 @@
 CREATE TABLE photos (
-    id SERIAL PRIMARY KEY,
-    url VARCHAR(200),
-    -- user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
-    user_id INTEGER REFERENCES users(id) ON DELETE SET NULL
+  id SERIAL PRIMARY KEY,
+  url VARCHAR(200),
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
+
 
 INSERT INTO photos (url, user_id)
 VALUES
-('http:/one.jpg', 4),
-('http:/five.jpg', 2),
-('http:/six.jpg', 3),
-('http:/seven.jpg', 4);
+  ('https://santina.net', 3),
+        ('https://alayna.net', 5),
+        ('https://kailyn.name', 3),
+        ('http://marjolaine.name', 1),
+        ('http://chet.net', 5),
+        ('http://jerrold.org', 2),
+        ('https://meredith.net', 4),
+        ('http://isaias.net', 4),
+        ('http://dayne.com', 4),
+        ('http://colten.net', 2),
+        ('https://adelbert.biz', 5),
+        ('http://kolby.org', 1),
+        ('https://deon.biz', 2),
+        ('https://marina.com', 5),
+        ('http://johnson.info', 1),
+        ('https://linda.info', 2),
+        ('https://tyrique.info', 4),
+        ('http://buddy.info', 5),
+        ('https://elinore.name', 2),
+        ('http://sasha.com', 3);
 
 
 SELECT * FROM photos;
-
-SELECT * FROM photos WHERE user_id = 4;
-
-SELECT url, username FROM photos JOIN users ON users.id = photos.user_id;
-
-DROP TABLE photos; -- To delete entire table
-
-DELETE FROM users
-WHERE id = 4;

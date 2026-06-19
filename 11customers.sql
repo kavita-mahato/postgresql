0b19220c6244d8af889ce6dd42826352aba1d315
Kavita Mahato <kavitamahato0835@gmail.com>
2026-06-19 19:56:52 +0530

    Subquery that return single value

--- 11customers.sql
+++ 11customers.sql
@@ -816,3 +816,16 @@ where price > (
 	FROM products
 	WHERE department = 'Toys'
 );
+
+----------- Subquery that results in a single value -------------
+SELECT name, price, (
+	SELECT MAX(price) FROM products
+)
+FROM products
+WHERE price > 867;
+
+SELECT name, price, (
+	SELECT price FROM products WHERE id = 3
+) AS id_3_price
+FROM products
+WHERE price > 867;

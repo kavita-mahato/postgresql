19729693d46aca261e0ca04e92e3e41b9f70a21e
Kavita Mahato <kavitamahato0835@gmail.com>
2026-07-02 18:23:43 +0530

    ALL operator in WHERE

--- 11customers.sql
+++ 11customers.sql
@@ -890,3 +890,12 @@ WHERE department NOT IN (
 	FROM products
 	WHERE price < 100
 );
+
+------------ Using ALL operator -------------
+SELECT name, department, price
+FROM products
+WHERE price > ALL (
+	SELECT price
+	FROM products
+	WHERE department = 'Industrial'
+);
